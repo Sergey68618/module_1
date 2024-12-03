76535f6294c278b88d853418c1983db726d104d6
Sergey68618 <sergey68618@gmail.com>
2024-12-03 23:57:03 +0500

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input("Ведите текст: ")
+print("В тексте", len(my_string), "символов.")
+# работа с методами строк
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])  # выводит первый символ строки
+print(my_string[-1])  # выводит последний символ строки
